84754ecdefc6768f5626135bb5d0fd290e01f7cc
Kanak Das <kdas006@ucr.edu>
2024-01-29 23:21:34 -0800

    fix paths

--- analysis/annotator.py
+++ analysis/annotator.py
@@ -1,13 +1,28 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+#     this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+#     the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import subprocess
 import os
 import shutil
 from pathlib import Path
 
-VERSION = '1.3.9-LOGISIM-SNAPSHOT'
+VERSION = '1.3.9-TAINT-SNAPSHOT'
+MODULE = 'core'
 REPO = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).strip().decode('utf-8')
-OUT_DIR = '{}/annotator-out/core'.format(REPO)
+OUT_DIR = '{}/annotator-out/{}'.format(REPO, MODULE)
 ANNOTATOR_JAR = "{}/.m2/repository/edu/ucr/cs/riple/annotator/annotator-core/{}/annotator-core-{}.jar".format(str(Path.home()), VERSION, VERSION)
-
 
 def prepare():
     os.makedirs(OUT_DIR, exist_ok=True)
@@ -20,14 +35,14 @@ def run_annotator():
     commands = []
     commands += ["java", "-jar", ANNOTATOR_JAR]
     commands += ['-d', OUT_DIR]
-    commands += ['-bc', 'cd {} && ./annotator-command.sh'.format(REPO)]
+    commands += ['-bc', 'cd {} && ./annotator-command.sh'.format(REPO, MODULE)]
     commands += ['-cp', '{}/paths.tsv'.format(OUT_DIR)]
     commands += ['-i', 'edu.ucr.Initializer']
     commands += ['-n', 'edu.ucr.cs.riple.taint.ucrtainting.qual.RUntainted']
     commands += ['-cn', 'UCRTaint']
-    commands += ["--depth", "7"]
+    commands += ["--depth", "10"]
     # Uncomment to see build output
-    commands += ['-rboserr']
+    # commands += ['-rboserr']
     # Comment to inject root at a time
     commands += ['-ch']
     # Uncomment to disable cache
