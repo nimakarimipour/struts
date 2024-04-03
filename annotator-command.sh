76d5b5ec4916c8b69f41bb891e8de031a550a770
Nima Karimipour <karimipour.nima@gmail.com>
2024-04-03 01:04:57 -0700

    update

--- annotator-command.sh
+++ annotator-command.sh
@@ -4,4 +4,4 @@ poly=true
 cf_version="3.42.0"
 ucrt_version="0.1-alpha-3-SNAPSHOT"
 rm -rvf core/target > /dev/null
-CF_VERSION=$cf_version UCRT_VERSION=$ucrt_version ANNOTATOR_POLY=$poly ANNOTATOR_LIBRARY=$lib ANNOTATOR_TYPE_ARG=$arg mvn compile -pl core
+CF_VERSION=$cf_version UCRT_VERSION=$ucrt_version ANNOTATOR_POLY=$poly ANNOTATOR_LIBRARY=$lib ANNOTATOR_TYPE_ARG=$arg /home/nima/Documents/environments/maven/apache-maven-3.9.1/bin/mvn compile -pl core
