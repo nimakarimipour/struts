044663c3a0ae8fa70aad03fb2756876aa754d183
Kanak Das <kdas006@ucr.edu>
2024-07-28 13:31:46 -0700

    depth 15

--- analysis/annotator.py
+++ analysis/annotator.py
@@ -25,7 +25,7 @@ def run_annotator():
     commands += ['-i', 'edu.ucr.Initializer']
     commands += ['-n', 'edu.ucr.cs.riple.taint.ucrtainting.qual.RUntainted']
     commands += ['-cn', 'UCRTaint']
-    commands += ["--depth", "25"]
+    commands += ["--depth", "15"]
     # Uncomment to see build output
     # commands += ['-rboserr']
     # Comment to inject root at a time
