f03e15c88136db595cb433fc4bbf17ecf01d5c22
Nima Karimipour <karimipour.nima@gmail.com>
2024-07-28 21:12:10 -0700

    update

--- annotator-command.sh
+++ annotator-command.sh
@@ -2,6 +2,6 @@ lib=true
 arg=true
 poly=true
 cf_version="3.42.0"
-ucrt_version="0.1-alpha-3-SNAPSHOT"
+ucrt_version="0.3-SNAPSHOT"
 rm -rvf core/target > /dev/null
 CF_VERSION=$cf_version UCRT_VERSION=$ucrt_version ANNOTATOR_POLY=$poly ANNOTATOR_LIBRARY=$lib ANNOTATOR_TYPE_ARG=$arg mvn compile -pl core
